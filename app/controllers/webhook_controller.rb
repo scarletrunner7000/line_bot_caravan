d8c30647d4a966e78d57366bd738438d32910cff
scarletrunner7000 <scarletrunner7000@gmail.com>
2016-12-04 17:30:03 +0900

    fix webhook#callback

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -25,6 +25,7 @@ class WebhookController < ApplicationController
             text: event.message['text']
           }
           resp = client.reply_message(event['replyToken'], message)
+          logger.debug request.to_hash
           logger.debug resp
           logger.debug resp.to_hash
         end
